9b4ed424efe4173022cc96d8b83f3ef79f3f31b0
varul <varul.srivastava@research.iiit.ac.in>
2024-01-29 02:52:47 +0530

    minor modifications to ratings abstract class

--- src/v0/rcls.py
+++ src/v0/rcls.py
@@ -1,27 +1,31 @@
 import elo 
 from rating_elo.elo import EloType,Elo
 
+'''
+    abstract class rating
+'''
 class Rating:
     def __init__(self,eloType=EloType.vanilla):
         self.players = {}
         self.elo = Elo(eloType)
+
+
     '''
         Abstract method; to be implemented in the derived class (depending on sport)
     '''
     def conduct_match(self,player_1:str,player_2:str,score:list,meta:dict):
         pass 
 
-    def change_rating(self,winner:str,loser:float):
-        new_ratings = elo.rate_1vs1(self.players[winner].rating,self.players[loser].rating)
-        self.players[winner] = new_ratings[0] 
-        self.players[loser] = new_ratings[1]
+    def change_rating(self,winner:str,loser:str,rwinner=1,rloser=0):
+        self.players[winner] = self.elo.elo_rate(self.players[winner],rwinner-rloser)
+        self.players[loser] = self.elo.elo_rate(self.players[loser],max(0,rloser-rwinner))
 
     def addPlayer(self,player_id,player_obj):
         if player_id in self.players.keys():
             print('ERROR: Player already exists')
         self.players[player_id] = player_obj
     
-    
+
 
 class Tennis(Rating):
     def __init__(self,rtype=EloType.vanilla):
