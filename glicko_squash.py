82fd560b744f6265b36221bcc28685deb3f912d2
varul <varul.srivastava@research.iiit.ac.in>
2024-03-05 00:54:47 +0530

    added changes to glicko dataset

--- glicko_squash.py
+++ glicko_squash.py
@@ -4,6 +4,8 @@ import sys
 # library imports 
 import os
 import pandas as pd 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import json
 from datetime import datetime
@@ -18,8 +20,12 @@ from src.Glicko2.glicko2 import Player
 from src.util.squash.score_sigmoid import score_sigmoid
 from src.util.squash.date_parser import date_parser
 
+from src.util.arparse import parseArguments
+from plotter import PlotEngine
+
+
 #load files 
-def load_data(fname):
+def load_data(fname,stdt=0,enddt=99999999):
     matches = pd.read_csv(fname)
     for ind,row in matches.iterrows():
         matches.at[ind,'date_time'] = int(date_parser(row['date_time']))
@@ -31,11 +37,24 @@ def load_data(fname):
 
     return matches
 
-def evaluateData(matches,p_ids):
+def date_to_timestamp(date:str): 
+    date = date.split('_')[0].split('-')
+    yr = int(date[0])
+    month = int(date[1])
+    day = int(date[0])
+    time_score = (yr - 1950)*365 + (month-1)*30 + day
+    timestamp = yr*10000 + int(month/12*100)*100 + int(day/(30 + month%2)*100)
+    return time_score, timestamp
+
+
+
+def evaluateData(matches,fname,p_ids):
     # print(matches.shape)
     # #print first few entries in the date_time column of matches
     dfs_to_concat = []
     df = pd.DataFrame()
+    prat = {} 
+    plerr = {} 
     player_ratings = {}
     for ind,row in tqdm(matches.iterrows(), total=len(matches), desc="Processing matches"):
         p1_id = row["usr_id"]
@@ -44,6 +63,9 @@ def evaluateData(matches,p_ids):
         for player_id in [p1_id, p2_id]:
             if player_id not in player_ratings:
                 player_ratings[player_id] = {"Rating": 1500, "RD": 200}
+            if player_id not in prat.keys():
+                prat[player_id] = {} 
+                plerr[player_id] = {} 
         
         p1_rating = player_ratings[p1_id]['Rating']
         p1_rd = player_ratings[p1_id]['RD']
@@ -67,27 +89,37 @@ def evaluateData(matches,p_ids):
             oc = min(oc1,oc2)
         else:
             oc = 0.5
+        ttime = row['date_time']/10000
+        timestamp = 10000*int(ttime/10000) + 100*int(100*((ttime%10000)/100)/12) + int(100*((ttime%100)/31))
 
         p1.update_player([p2_rating],[p2_rd],[oc])
         p2.update_player([p1_rating],[p1_rd],[1-oc])
         
         player_ratings[p1_id]['Rating'] = p1.getRating()
+        prat[p1_id][timestamp] = p1.getRating()
         player_ratings[p1_id]['RD'] = p1.getRd()
         player_ratings[p2_id]['Rating'] = p2.getRating()
+        prat[p2_id][timestamp] = p2.getRating()
         player_ratings[p2_id]['RD'] = p2.getRd()
-
-
-        if row['usr_id'] in p_ids:
+        
+        if True: # don't need filtration here..
             # Create a dataframe with the required data
             player_df = pd.DataFrame({'player_id': [row['usr_id']], 'player_rating': [player_ratings[row['usr_id']]['Rating']], 'tourney_date': [row['date_time']]})
             # Append the dataframe to the list
             dfs_to_concat.append(player_df)
 
-        if row['oppnt_id'] in p_ids:
+        if True: # don't need filtration here...
             # Create a dataframe with the required data
             player_df = pd.DataFrame({'player_id': [row['oppnt_id']], 'player_rating': [player_ratings[row['oppnt_id']]['Rating']], 'tourney_date': [row['date_time']]})
             # Append the dataframe to the list
             dfs_to_concat.append(player_df)
+
+    masterdict = {
+        'rating':prat,
+        'error':plerr
+    }
+    with open(fname,'w') as fp:
+        json.dump(masterdict,fp)
 
     # Concatenate all dataframes in the list
     if dfs_to_concat:
@@ -121,21 +153,20 @@ def display_results(dataset):
     plt.show()
 
 if __name__ == '__main__':
-    # fname = input('Enter Dataset Filename')
-    fname = sys.argv[1]
-    dformat = sys.argv[2]
-    assert(dformat in ['datewise','matchwise'])
-    dataset = load_data(fname)
-    if len(sys.argv) == 4: # input file 
-        try:
-            input_set = json.load(open(sys.argv[3],'r'))['input']
-        except Exception as e:
-            print('Error: Invalid file or input format. Please input list through a json file.')
-            print(e)
-            exit(1)   
-    else:
-        print("Glicko needs input file. Please input list through a json file.")
-        exit(1)
+    
+    arguments = parseArguments(sys.argv)
+    plotter = PlotEngine(arguments.display,arguments.plot_path)
 
-    preds = evaluateData(dataset,input_set)
-    display_results(preds)
+    stdt = arguments.startTime 
+    endt = arguments.endTime 
+    subFilter = None 
+    if arguments.input != None:
+        with open(arguments.input,'r') as fp:
+            subFilter = json.load(fp)['inputs']
+
+    if arguments.train == 1:
+        dataset = load_data(arguments.dataset,stdt,endt)
+        preds = evaluateData(dataset,arguments.output,subFilter)
+    plotter.load_data(arguments.output)
+    plotter.plot_ratings(subFilter,arguments.percentage)
+
